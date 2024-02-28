a2d4badc9f419dca43eed492b00b4d7afb4afa39
Olliemyers4 <olliemyers4@gmail.com>
2024-02-28 20:25:33 +0000

    Made code more readable + Added progress bar

--- tiktokComments.py
+++ tiktokComments.py
@@ -8,24 +8,35 @@ import time
 import os
 import re
 import json
+from tqdm import tqdm
 
+# --- CONFIG SETTINGS ---
+
+# Load the config file (only contains the chrome data path for now)
 configFile = open("config.json","r").read()
 config = json.loads(configFile)
-
 dataPath = "--user-data-dir=" + config['chromeDataPath']
+
+# Define options for the Chrome Driver
 options = Options()
-options.headless = True
-options.add_argument("--window-size=1920,1200")
-options.add_argument(dataPath)
+options.add_argument("--window-size=1920,1200") # Set the window size
+options.add_argument(dataPath) # Set the user data directory (to keep the user logged in on TikTok)
+
+# Stanard options (Helps to make the program look less like a bot)
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_argument("--disable-blink-features")
 options.add_argument('--disable-blink-features=AutomationControlled')
 options.add_experimental_option('useAutomationExtension', False)
 options.add_argument("--lang=en-GB,en-US,en")
-options.add_argument("--log-level=3")
+options.add_argument("--log-level=3") # Set the log level to 3 to stop the console from being spammed from the warnings - It will only show errors
+
+# Create the Chrome Driver
 driver = webdriver.Chrome(options=options)
 
+# Define the user agent of the bot (Helps to make the program look less like a bot)
 driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+
+# The stealth module is an extra measure to make this look less like a bot
 stealth(driver,
         languages=["en-US", "en"],
         vendor="Google Inc.",
@@ -35,40 +46,49 @@ stealth(driver,
         fix_hairline=True,
         )
 
-#print(driver.execute_script("return navigator.userAgent;"))
+# --- END OF CONFIG SETTINGS ---
 
-#get videos from file
+
+
 videos = []
-tag = input("Enter tag: ")
 
-tic = time.perf_counter()
+# Get the tag from the user
+tag = input("Enter tag: ")
+print("Loading Tag...")
+
 driver.get(f"https://www.tiktok.com/tag/{tag}")
 
 try:
     element = WebDriverWait(driver, 120).until(
         EC.presence_of_element_located((By.XPATH, '//div[contains(@class,"css-wjuodt-DivVideoFeedV2 ecyq5ls0")]'))
     )
-    toc = time.perf_counter()
-    #print(f"Page Loaded in {toc-tic:0.4f} seconds")
 except:
+    # If the page fails to load, take a screenshot and close the program
     print("Page Failed")
     driver.get_screenshot_as_file(os.curdir + "\\failed1.png")
     time.sleep(0.5)
     driver.get_screenshot_as_file(os.curdir + "\\failed2.png")
+    driver.quit()
+    exit(1)
 
 time.sleep(5)
 
 htmlSource = str(driver.page_source)
 links = [m.start() for m in re.finditer('https://www.tiktok.com/@', htmlSource)]
 seen = []
+
+# Get the links to the videos - It will find each link twice so duplicates will be removed
 for i in range(len(links)):
     link = htmlSource[links[i]:htmlSource.find('"',links[i])]
     video = "https://www.tiktok.com/@" + link 
     if video not in seen:
         seen.append(video)
 
+
+print("Loading Comments...")
+
 with open("comments.txt","wb") as f:
-    for video in seen:
+    for video in tqdm(seen):
         tic = time.perf_counter()
         driver.get(video)
 
@@ -77,7 +97,7 @@ with open("comments.txt","wb") as f:
                 EC.presence_of_element_located((By.XPATH, '//div[contains(@class,"DivCommentBarContainer")]'))
             )
             toc = time.perf_counter()
-            print(f"Page Loaded in {toc-tic:0.4f} seconds")
+            #print(f"Page Loaded in {toc-tic:0.4f} seconds")
         except:
             print("Page Failed")
             driver.get_screenshot_as_file(os.curdir + "\\failed1.png")
@@ -87,20 +107,14 @@ with open("comments.txt","wb") as f:
 
 
         #Now we have the page loaded, we can get the comments from this div class css-g6odvl-DivCommentContainer ekjxngi0
-        #comments = driver.find_elements(By.XPATH,'//div[contains(@class,"DivCommentListContainer")]')
-        #get children of this div
-        #parent = comments[0]
+
         children = driver.find_elements(By.XPATH,'.//div[contains(@class,"DivCommentItemContainer")]')
-        #foreach child, get the username and comment
         f.write(("\n").encode('utf-8'))
         f.write(("Video: " + video + "\n").encode('utf-8'))
         f.write(("Comments:\n").encode('utf-8'))
 
         for i in range(len(children)):
             child = children[i]
-            #print(child.find_elements(By.XPATH,'.//span[contains(@class,"SpanUserNameText")]')[0].text + ":")
-            #print(child.find_elements(By.XPATH,'.//p[contains(@class,"PCommentText")]')[0].text)
-            #print("--------------------")
             f.write((child.find_elements(By.XPATH,'.//span[contains(@class,"SpanUserNameText")]')[0].text + ":\n").encode('utf-8'))
             f.write((child.find_elements(By.XPATH,'.//p[contains(@class,"PCommentText")]')[0].text + "\n").encode('utf-8'))
             f.write(("--------------------\n").encode('utf-8'))
